fea61aba2eb5a1d6d683970d05aad32e57b43458
Yishar Piero <204321@unsaac.edu.pe>
2024-05-16 14:40:17 -0500

    Kruskal

---
+++ Algorithms and Data Structures/Graphs/Kruskal.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h> 
+using namespace std;
+
+const int N = 1e6;
+
+vector<int> parent(N), rank(N);
+
+void make_set(int v) {
+    parent[v] = v;
+    rank[v] = 0;
+}
+
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}
+
+struct Edge {
+    int u, v, weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight;
+    }
+};
+
+int n;
+vector<Edge> edges;
+
+int cost = 0;
+vector<Edge> result;
+parent.resize(n);
+rank.resize(n);
+for (int i = 0; i < n; i++)
+    make_set(i);
+
+sort(edges.begin(), edges.end());
+
+for (Edge e : edges) {
+    if (find_set(e.u) != find_set(e.v)) {
+        cost += e.weight;
+        result.push_back(e);
+        union_sets(e.u, e.v);
+    }
+}
